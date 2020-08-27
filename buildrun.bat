1ac9594caf421d38b57ef8ac18592a7ddaac0d16
Mikael Chudinov <mikael@chudinov.net>
2020-08-28 01:42:35 +0200

    Generate certs on the fly.

--- buildrun.bat
+++ buildrun.bat
@@ -6,6 +6,8 @@ docker-compose up -d
 
 docker-compose down
 
+docker build -t vault .
+
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout localhost.key -out localhost.crt -config localhost.conf
 
 https://github.com/minio/minio-boshrelease
@@ -13,3 +15,5 @@ $ bosh deploy -d minio manifests/manifest-dist-example.yml \
     -v minio_deployment_name=minio \
     -v minio_accesskey=admin \
     -v minio_secretkey=CHANGEME!
+
+
